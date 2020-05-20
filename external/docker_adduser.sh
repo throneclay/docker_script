93028e5501ac17bda7f6ab289142a02bb47b4ba6
throneclay <throneclay@gmail.com>
2020-05-20 14:34:05 +0800

    update bash

--- external/docker_adduser.sh
+++ external/docker_adduser.sh
@@ -5,3 +5,5 @@ adduser --disabled-password --force-badname --gecos '' "$DOCKER_USER" \
     --uid "$DOCKER_USER_ID" --gid "$DOCKER_GRP_ID" 2>/dev/null
 usermod -aG sudo "$DOCKER_USER"
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo "alias ls=\"ls --color=auto\"" >> .bashrc
+chown $DOCKER_USER: -R /home/$DOCKER_USER
