4c71a1d238d0b0616f6ec1ffdf51c98ca82191b5
throneclay <throneclay@gmail.com>
2021-01-22 13:54:32 +0800

    add user tags in docker name

--- dinto.sh
+++ dinto.sh
@@ -5,7 +5,7 @@ dir_name="$(pwd | rev | awk -F \/ '{print $1}' | rev)"
 if [ $# -eq 1 ]; then
   docker_name="$1"
 else
-  docker_name="$dir_name"
+  docker_name="$dir_name_${USER}"
 fi
 
 docker exec -u ${USER} -it $docker_name /bin/bash
